423063e84334262288c1b1113cad11ec392cadc0
Zhang-Zichen <zac13208075114@163.com>
2025-10-21 18:00:18 +0800

    Delete db.js

--- db.js
+++
@@ -1,7 +0,0 @@
-// 替换为你的InfinityFree数据库信息
-module.exports = {
-  host: "sql206.infinityfree.com", // 数据库主机
-  user: "if0_40191281", // 数据库用户名
-  password: "2hc74w0zR4D08G6", // 数据库密码
-  database: "if0_40191281_Zac" // 数据库名
-};
